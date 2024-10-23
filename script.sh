70b3600ab09edeeb1cdaf25ee946262db1a5d966
Andrew Hodges <hodgeand@mail.gvsu.edu>
2024-10-22 23:50:12 -0400

    working script with correct output files

--- script.sh
+++ script.sh
@@ -2,7 +2,10 @@ sizes=(8 16 32)
 
 for program in $@
 do	
-	python3 $program ${sizes[0]} > $program.out
-	python3 $program ${sizes[1]} >> $program.out
-	python3 $program ${sizes[2]} >> $program.out
+	in_name=${program}.py
+	out_name=$(echo ${program} | cut -d '.' -f 1 )
+	
+	python3 $in_name ${sizes[0]} > $out_name.out
+	python3 $in_name ${sizes[1]} >> $out_name.out
+	python3 $in_name ${sizes[2]} >> $out_name.out
 done
